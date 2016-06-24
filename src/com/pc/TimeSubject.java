b437c077e309965ea24bd18acddd5cb756e8af1a
Switch <q547550831@outlook.com>
2016-06-24 21:01:18 +0800

    Created and wrote class TimeSubject according to the class diagram.

--- src/com/pc/TimeSubject.java
+++ src/com/pc/TimeSubject.java
@@ -4,16 +4,33 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Created by Administrator on 2016-06-24.
+ * Created by Switch on 2016-06-24.
  */
 public abstract class TimeSubject {
+    /**
+     * 时钟绑定对象
+     */
     protected Map<String, Clock> clocks = new HashMap<String, Clock>();
 
+    /**
+     * 附加城市和时钟
+     * @param cityName 城市名
+     * @param clock 时钟
+     */
     public void attach(String cityName, Clock clock) {
         clocks.put(cityName, clock);
     }
 
+    /**
+     * 解除绑定
+     * @param cityName 城市名
+     */
     public void detach(String cityName) {
         clocks.remove(cityName);
     }
+
+    /**
+     * 提示所有时钟
+     */
+    public abstract void notifyAllClocks();
 }
