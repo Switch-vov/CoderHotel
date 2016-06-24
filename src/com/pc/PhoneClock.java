2e17fe17fc4b89633d12b806b1fe068d96dc3fbb
Switch <q547550831@outlook.com>
2016-06-24 21:39:26 +0800

    Made field Clock.localTime and Clock.UTC_OFFSET protected.

--- src/com/pc/PhoneClock.java
+++ src/com/pc/PhoneClock.java
@@ -7,7 +7,7 @@ public class PhoneClock extends Clock{
 
     @Override
     public void setLocalTime(int localTime) {
-        this.localTime = localTime;
+        super.localTime = localTime;
         this.utcTime.setUtcZeroTime(localTime - UTC_OFFSET);
     }
 }
