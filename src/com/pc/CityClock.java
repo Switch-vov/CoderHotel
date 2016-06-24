59a95feac39cca5a0804c73e7942dad03cf17e40
Switch <q547550831@outlook.com>
2016-06-24 21:44:32 +0800

    Implemented method CityClock.setLocalTime().

--- src/com/pc/CityClock.java
+++ src/com/pc/CityClock.java
@@ -3,5 +3,10 @@ package com.pc;
 /**
  * Created by  on 2016-06-24.
  */
-public class CityClock {
+public class CityClock extends Clock{
+
+    @Override
+    public void setLocalTime(int localTime) {
+        super.localTime = localTime;
+    }
 }
