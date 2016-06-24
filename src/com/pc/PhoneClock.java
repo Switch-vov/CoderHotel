262d902c1f2cab64688a98319bc09ebb81687611
Switch <q547550831@outlook.com>
2016-06-24 21:34:42 +0800

    Implemented method PhoneClock.setLocalTime() according to class diagram.

--- src/com/pc/PhoneClock.java
+++ src/com/pc/PhoneClock.java
@@ -3,6 +3,11 @@ package com.pc;
 /**
  * Created by Switch on 2016-06-24.
  */
-public class PhoneClock {
+public class PhoneClock extends Clock{
 
+    @Override
+    public void setLocalTime(int localTime) {
+        this.localTime = localTime;
+        this.utcTime.setUtcZeroTime(localTime - UTC_OFFSET);
+    }
 }
