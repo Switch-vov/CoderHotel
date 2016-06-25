9d074f78be9b0b1a02067f28bf2bc54937e80b1e
Switch <q547550831@outlook.com>
2016-06-25 15:29:07 +0800

    Added the main() method to class HotelWorldClocksRunner and wrote the expected code there.

--- src/com/pc/HotelWorldClocksRunner.java
+++ src/com/pc/HotelWorldClocksRunner.java
@@ -4,4 +4,21 @@ package com.pc;
  * Created by Switch on 2016-06-25.
  */
 public class HotelWorldClocksRunner {
+    public static void main(String[] args) {
+        // 初始化
+        TimeSubject utcTime = new UtcTime();
+        utcTime.attach("beijing", new CityClock(8));
+        utcTime.attach("london", new CityClock(0));
+        utcTime.attach("moscow", new CityClock(4));
+        utcTime.attach("sydney", new CityClock(10));
+        utcTime.attach("newYork", new CityClock(-5));
+        Clock phoneClock = new PhoneClock(utcTime);
+
+        // 设置手机时间
+        phoneClock.setLocalTime(9);
+
+        // 打印所有时钟
+        utcTime.printTimeOfAllClocks();
+
+    }
 }
