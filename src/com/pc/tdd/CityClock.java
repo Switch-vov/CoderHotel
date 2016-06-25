ce3016e157cd30165ca02abb557c96cf436a5d61
Switch <q547550831@outlook.com>
2016-06-25 17:23:12 +0800

    Replaced the fake value with the intention code in method CityClock.getTime().

--- src/com/pc/tdd/CityClock.java
+++ src/com/pc/tdd/CityClock.java
@@ -4,11 +4,13 @@ package com.pc.tdd;
  * Created by Switch on 2016-06-25.
  */
 public class CityClock {
+    // TODO: The constructors of CityClock and PhoneClock are duplicated
     public CityClock(int utcOffset) {
 
     }
 
     public int getTime() {
-        return 1;
+        // TODO-working-on: The fake value in the production code and the expected value in the test code are duplicated
+        return this.utcOffset + this.utcZeroTime;
     }
 }
