b884120d0d92254fc255c0f80ddfe8dc835f4a9b
Switch <q547550831@outlook.com>
2016-06-25 19:00:10 +0800

    Created method PhoneClock.setHotelWorldClockSystem().

--- src/com/pc/tdd/PhoneClock.java
+++ src/com/pc/tdd/PhoneClock.java
@@ -20,4 +20,8 @@ public class PhoneClock {
     public void setTime(int time) {
         this.cityClock.setUtcZeroTime(time - this.utcOffset);
     }
+
+    public void setHotelWorldClockSystem(HotelWorldClockSystem hotelWorldClockSystem) {
+
+    }
 }
