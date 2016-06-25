314b58c6403501baf3dcb5ea6c18b411a3b64889
Switch <q547550831@outlook.com>
2016-06-25 17:56:19 +0800

    Added test the_time_of_clock_NewYork_should_be_20_after_the_phone_clock_is_set_to_9_Beijing_time() and got a negative hour in the actual result.

--- src/com/pc/tdd/HotelWorldClocksTest.java
+++ src/com/pc/tdd/HotelWorldClocksTest.java
@@ -8,6 +8,7 @@ import static org.junit.Assert.assertEquals;
  * Created by Switch on 2016-06-25.
  */
 public class HotelWorldClocksTest {
+
     @Test
     public void the_time_of_clock_London_should_be_1_after_the_phone_clock_is_set_to_9_Beijing_time() {
         // Arrange
@@ -20,5 +21,19 @@ public class HotelWorldClocksTest {
 
         // Assert
         assertEquals(1, londonClock.getTime());
+    }
+    // TODO-working-on: Negative hour
+    @Test
+    public void the_time_of_clock_NewYork_should_be_20_after_the_phone_clock_is_set_to_9_Beijing_time() {
+        // Arrange
+        CityClock newYorkClock = new CityClock(-5);
+        PhoneClock phoneClock = new PhoneClock(8);
+
+        // Act
+        phoneClock.setCityClock(newYorkClock);
+        phoneClock.setTime(9);
+
+        // Assert
+        assertEquals(20, newYorkClock.getTime());
     }
 }
