896032d15950c8769b9e23e13e90a12256e4d727
Switch <q547550831@outlook.com>
2016-06-25 15:32:00 +0800

    Updated the main() method to make the constructor of PhoneClock is the same with CityClock and add the UtcTime object to the phoneClock using method PhoneClock.setUtcTime().

--- src/com/pc/HotelWorldClocksRunner.java
+++ src/com/pc/HotelWorldClocksRunner.java
@@ -12,7 +12,9 @@ public class HotelWorldClocksRunner {
         utcTime.attach("moscow", new CityClock(4));
         utcTime.attach("sydney", new CityClock(10));
         utcTime.attach("newYork", new CityClock(-5));
-        Clock phoneClock = new PhoneClock(utcTime);
+
+        Clock phoneClock = new PhoneClock(8);
+        phoneClock.setUtcTime(utcTime);
 
         // 设置手机时间
         phoneClock.setLocalTime(9);
