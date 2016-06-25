4bab7304ad35877e719fbe6c196955fbd1eacdba
Switch <q547550831@outlook.com>
2016-06-25 17:31:29 +0800

    Created field cityClock and assigned a value to it in class PhoneClock.

--- src/com/pc/tdd/PhoneClock.java
+++ src/com/pc/tdd/PhoneClock.java
@@ -1,16 +1,19 @@
 package com.pc.tdd;
 
+
 /**
  * Created by Switch on 2016-06-25.
  */
 public class PhoneClock {
+
+    private CityClock cityClock;
 
     public PhoneClock(int utcOffset) {
 
     }
 
     public void setCityClock(CityClock cityClock) {
-
+        this.cityClock = cityClock;
     }
 
     public void setTime(int time) {
