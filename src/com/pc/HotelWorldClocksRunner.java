12c5db15eda597627494b981decb5386fb819b5e
Switch <q547550831@outlook.com>
2016-06-25 15:40:06 +0800

    Changed type of variable phoneClock in main() to be PhoneClock.

--- src/com/pc/HotelWorldClocksRunner.java
+++ src/com/pc/HotelWorldClocksRunner.java
@@ -13,7 +13,7 @@ public class HotelWorldClocksRunner {
         utcTime.attach("sydney", new CityClock(10));
         utcTime.attach("newYork", new CityClock(-5));
 
-        Clock phoneClock = new PhoneClock(8);
+        PhoneClock phoneClock = new PhoneClock(8);
         phoneClock.setUtcTime(utcTime);
 
         // 设置手机时间
