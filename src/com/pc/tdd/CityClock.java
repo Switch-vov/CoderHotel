c887fa092f8da2887230ff0130076707464849a1
Switch <q547550831@outlook.com>
2016-06-25 19:32:59 +0800

    Made class CityClock use the field utcOffset from the super class Clock.

--- src/com/pc/tdd/CityClock.java
+++ src/com/pc/tdd/CityClock.java
@@ -4,16 +4,15 @@ package com.pc.tdd;
  * Created by Switch on 2016-06-25.
  */
 public class CityClock extends Clock{
-    private int utcOffset;
     private int utcZeroTime;
 
     // TODO: The constructors of CityClock and PhoneClock are duplicated
     public CityClock(int utcOffset) {
-        this.utcOffset = utcOffset;
+        super.utcOffset = utcOffset;
     }
 
     public int getTime() {
-        return (this.utcOffset + this.utcZeroTime + 24) % 24;
+        return (super.utcOffset + this.utcZeroTime + 24) % 24;
     }
 
     public void setUtcZeroTime(int utcZeroTime) {
