4cfd1af1c0d9c089f5924dd10492daa3582d9b32
Switch <q547550831@outlook.com>
2016-06-25 16:35:16 +0800

    Added test the_time_of_clock_London_should_be_1_after_the_phone_clock_is_set_to_9_Beijing_time() adn wrote an assertion.

---
+++ src/com/pc/HotelWorldClocksTest.java
@@ -0,0 +1,18 @@
+package com.pc;
+
+import org.junit.Test;
+
+/**
+ * Created by Switch on 2016-06-25.
+ */
+public class HotelWorldClocksTest {
+    @Test
+    public void the_time_of_clock_London_should_be_1_after_the_phone_clock_is_set_to_9_Beijing_time() {
+        // Arrange
+
+        // Act
+
+        // Assert
+        assertEquals(1, londonClock.getTime());
+    }
+}
