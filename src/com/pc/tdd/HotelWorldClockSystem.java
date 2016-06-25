ba6838fb2a96a53041cab5d9cf8e90651e423140
Switch <q547550831@outlook.com>
2016-06-25 19:08:09 +0800

    Created method HotelWorldClockSystem.getClocks() to return a clock list stored in this class.

--- src/com/pc/tdd/HotelWorldClockSystem.java
+++ src/com/pc/tdd/HotelWorldClockSystem.java
@@ -1,10 +1,16 @@
 package com.pc.tdd;
 
+import java.util.ArrayList;
+
 /**
  * Created by Switch on 2016-06-25.
  */
 public class HotelWorldClockSystem {
     public void attach(CityClock cityClock) {
 
+    }
+
+    public ArrayList<CityClock> getClocks() {
+        return this.cityClocks;
     }
 }
