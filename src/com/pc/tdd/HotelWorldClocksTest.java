56e5cfb5a7f5df7068cbfb192b9c9c5664173a9d
Switch <q547550831@outlook.com>
2016-06-25 18:49:04 +0800

    Added test the_time_of_clock_London_and_NewYork_should_be_1_and_20_respectively_after_the_the_phone_clock_is_set_to_9_Beijing_time() and wrote 2 assertions.

--- src/com/pc/tdd/HotelWorldClocksTest.java
+++ src/com/pc/tdd/HotelWorldClocksTest.java
@@ -22,7 +22,7 @@ public class HotelWorldClocksTest {
         // Assert
         assertEquals(1, londonClock.getTime());
     }
-    // TODO-working-on: Negative hour
+
     @Test
     public void the_time_of_clock_NewYork_should_be_20_after_the_phone_clock_is_set_to_9_Beijing_time() {
         // Arrange
@@ -36,4 +36,14 @@ public class HotelWorldClocksTest {
         // Assert
         assertEquals(20, newYorkClock.getTime());
     }
+
+    // TODO-working-on: Set time to multiple city clocks
+    @Test
+    public void the_time_of_clock_London_and_NewYork_should_be_1_and_20_respectively_after_the_the_phone_clock_is_set_to_9_Beijing_time() {
+
+        // Assert
+        assertEquals(1, londonClock.getTime());
+        assertEquals(20, newYorkClock.getTime());
+    }
+
 }
