0b6e94948e1f7f35fe0169f3675c3a7e91a8817e
Switch <q547550831@outlook.com>
2016-06-25 19:12:40 +0800

    Created and initialized field cityClocks in class HotelWorldClockSystem.

--- src/com/pc/tdd/HotelWorldClockSystem.java
+++ src/com/pc/tdd/HotelWorldClockSystem.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
  * Created by Switch on 2016-06-25.
  */
 public class HotelWorldClockSystem {
+    private ArrayList<CityClock> cityClocks = new ArrayList<>();
+
     public void attach(CityClock cityClock) {
 
     }
